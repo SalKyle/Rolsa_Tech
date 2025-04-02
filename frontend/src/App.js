a2724135f8e5eebeb46e9b2ad1b0f850d8e3abe7
SalKyle <106779106+SalKyle@users.noreply.github.com>
2025-04-02 10:33:17 +0100

    footer styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,16 +8,13 @@ import CfPage from "./pages/cf_page";
 import EVLocator from "./pages/EVLocator";
 import EnergyTracker from "./pages/EnergyTracker";
 import BookingPage from "./pages/BookingPage";
-import Dashboard from "../src/pages/components/Dashboard";
 import Products from "./pages/ProductsPage";
-import { AuthContext } from "./context/AuthContext"; 
+import { AuthContext } from "./context/AuthContext";
 import Footer from "./pages/components/Footer";
 
 const App = () => {
-  // State to manage user information
   const [user, setUser] = useState(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
-    // return storedUser?.token ? storedUser : null;
     return storedUser || null;
   });
 
@@ -29,38 +26,34 @@ const App = () => {
     }
   }, [user]);
 
-  // Error handler
   const onError = (error) => {
     console.error("Error:", error);
     alert("An error occurred. Please try again.");
   };
 
   return (
-    <div>
-      <AuthContext.Provider value={{ currentUser: user, setCurrentUser: setUser }}>
-        <Router>
-          <Routes>
-            <Route path="/signup" element={<Signup setUser={setUser} onError={onError} />} />
-            <Route path="/login" element={<Login setUser={setUser} onError={onError} />} />
-            <Route path="/landing" element={<LandingPage />} />
-            <Route path="/cf_page/*" element={<CfPage />} />
-            <Route path="/Products" element={<Products />} />
-            <Route path="/EVLocator" element={<EVLocator />} />
-            <Route path="/BookingPage" element={<BookingPage />} />
-            <Route path="/EnergyTracker" element={<EnergyTracker />} />
-            {/* <Route path="/Dashboard" element={<Dashboard />} /> */}
-            
-            <Route
-              path="/"
-              element={user ? <HomePg user={user} /> : <Navigate to="/landing" />}
-            />
-          </Routes>
-        </Router>
-      </AuthContext.Provider>
-      <Footer />
-    </div>
+    <AuthContext.Provider value={{ currentUser: user, setCurrentUser: setUser }}>
+      <Router>
+        <div className="app-container">
+          <div className="app-content">
+            <Routes>
+              <Route path="/signup" element={<Signup setUser={setUser} onError={onError} />} />
+              <Route path="/login" element={<Login setUser={setUser} onError={onError} />} />
+              <Route path="/landing" element={<LandingPage />} />
+              <Route path="/cf_page/*" element={<CfPage />} />
+              <Route path="/Products" element={<Products />} />
+              <Route path="/EVLocator" element={<EVLocator />} />
+              <Route path="/BookingPage" element={<BookingPage />} />
+              <Route path="/EnergyTracker" element={<EnergyTracker />} />
+              {/* <Route path="/Dashboard" element={<Dashboard />} /> */}
+              <Route path="/" element={user ? <HomePg user={user} /> : <Navigate to="/landing" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
 export default App;
-
