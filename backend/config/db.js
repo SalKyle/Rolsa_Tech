3598c2345e41dd0490ae079217f7dba40247b887
SalKyle <106779106+SalKyle@users.noreply.github.com>
2025-05-06 11:26:27 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3').verbose();
+// config/db.jsconst sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./rolsa.db', (err) => {
   if (err) console.error('Failed to connect to DB:', err.message);
   else console.log('SQLite Database connected');
